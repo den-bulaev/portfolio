e5a9981b6674b5ea1c171dd9369a5c0d342ce5c0
den-bulaev <denisbulaev7@gmail.com>
2024-12-07 19:54:53 +0200

    card link updated

--- src/utils.ts
+++ src/utils.ts
@@ -25,7 +25,7 @@ export const appCards: IAppCard[] = [
     image: binancePreview,
     title: "Binance API Integration",
     linkToDemo: "https://den-bulaev.github.io/smart-business-test-task/",
-    linkToCode: "https://github.com/den-bulaev/Museum",
+    linkToCode: "https://github.com/den-bulaev/smart-business-test-task",
     stack: "react, redux toolkit, tanstack table, html, css",
   },
   {

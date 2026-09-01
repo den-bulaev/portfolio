7a8ec9960d82735154b6065ebde9540901a79c24
den-bulaev <denisbulaev7@gmail.com>
2026-09-01 16:20:37 +0300

    version added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import { appCards } from "./utils";
+import { useEffect, useState } from "react";
+
 import CardsGrid from "./components/CardsGrid/CardsGrid";
+import { appCards, appCardsChrome, ESiteVersion, getVersion } from "./utils";
 
 function App() {
+  const [siteVersion, setSiteVersion] = useState("");
+
+  useEffect(() => {
+    const version = getVersion();
+
+    if (version === ESiteVersion.CHROME) {
+      setSiteVersion(version);
+    }
+  }, []);
+
   return (
     <main>
-      <h1>Portfolio</h1>
-      <CardsGrid cards={appCards} />
+      <h1>
+        {siteVersion === ESiteVersion.CHROME
+          ? "Web Store Portfolio"
+          : "Portfolio"}
+      </h1>
+      <CardsGrid
+        cards={siteVersion === ESiteVersion.CHROME ? appCardsChrome : appCards}
+      />
     </main>
   );
 }
